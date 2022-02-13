43b295d55e305c6d08f6f204e407107d2341d17e
Abilay Aytbanov <aitbanov640@gmail.com>
2022-02-13 21:42:44 +0600

    IOS-Labs

---
+++ Labs/Lab - Introduction.playground/Pages/1. Exercise - Use Playgrounds.xcplaygroundpage/Contents.swift
@@ -0,0 +1,36 @@
+/*:
+ ## Exercise - Use Playgrounds
+
+ The code below prints a few short statements about what you have learned in this lesson. Open the console area and view the code's output.
+ */
+print("I have learned the following:")
+print("What features make Swift a modern and safe language")
+print("How to use the Swift REPL in Terminal")
+print("How to use playgrounds to make writing Swift fun and simple")
+/*:
+ Now print your own phrases to the console. Pick one of your favorite songs. Use your knowledge of the `print` function to display the song title and artist.
+ */
+print("The Artist:Bob Marley")
+print("The Title of the song:Redemption Song")
+
+/*:
+ Use multiple `print` functions to write out some of the lyrics to the song.
+ */
+print("Emancipate yourselves from mental slavery")
+print("None but ourselves can free our minds")
+print("Have no fear for atomic energy")
+print("Cause none of them can stop the time")
+print("How long shall they kill our prophets")
+print("While we stand aside and look? Ooh")
+print("Some say it's just a part of it")
+print("We've got to fullfil the book")
+/*:
+
+ _Copyright © 2018 Apple Inc._
+
+ _Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:_
+
+ _The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software._
+
+ _THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE._
+ */
---
+++ Labs/Test.playground/Contents.swift
@@ -0,0 +1,45 @@
+import UIKit
+
+//var greeting = "Hello, playground"
+//print(greeting)
+//
+//
+//var a=5
+//var b=2
+//
+//print(a/b)
+//
+//var b1:String="Gello"
+//print(b1)
+//
+//var d:Double=123.2
+//print(d/2)
+//
+//
+//var a1:Int=12
+//print(String(12))
+
+//struct Person{->type definition(struct{})
+//    let firstName:String //type annotation
+//    let lastName:String
+//
+//    func sayHello(){
+//        print("Hello there my name is \(firstName) and my last name is \(lastName)!")
+//    }
+//
+//}
+//
+//let aPerson=Person(firstName:"Abylai", lastName:"Aitbanov")
+//
+//aPerson.sayHello()
+
+//var largeUglyNumber = 1000000000
+//var largePrettyNumber = 1_000_000_000
+//
+//print(largePrettyNumber)
+//
+//
+//let firstName:String
+//
+//firstName="Harry"
+//print(firstName)
